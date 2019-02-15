afd35cc80e5ebcf5d7c505a79e12a9a70bd1e058
thiyaguanbu <thethiyagu@yahoo.com>
2019-02-15 14:44:22 +0530

    Add files via upload

---
+++ SpringBootSwagger/src/test/java/com/springbootswagger/example/SpringBootSwaggerApplicationTests.java
@@ -0,0 +1,19 @@
+package com.springbootswagger.example;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSwaggerApplicationTests {
+	
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}
